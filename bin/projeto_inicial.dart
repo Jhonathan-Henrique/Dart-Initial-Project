bc192e7626bb86ddc4472e561c733dcc496ecaae
Jhonathan <Jhonathan.Engineer@gmail.com>
2024-11-24 09:08:22 -0300

    chore: add list

--- bin/projeto_inicial.dart
+++ bin/projeto_inicial.dart
@@ -1,11 +1,21 @@
 void main () {
+
+
+
   int idade = 29;
   double altura = 1.88;
   bool geek = true;
   String nome = 'Jhonathan';
-  String frase = 'Eu sou o $nome \n'
-  'Eu me considero Geek: $geek \n'
-  'tenho $idade anos e $altura de altura';
+
+  List<dynamic> john = [29, 1.88, true, 'Jhonathan'];
+
+  String frase = 'Eu sou o ${john[3]} \n'
+  'Eu me considero Geek: ${john[2]} \n'
+  'tenho ${john[0]} anos e ${john[1]} de altura';
+  List<String> listaNomes = ['Jhonathan', 'Janny', 'Vera', 'Dida'];
+
+
+
 
   print(frase);
 }
