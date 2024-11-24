5cdcbb59520a8e77d52109d15308cad36179a427
Jhonathan <Jhonathan.Engineer@gmail.com>
2024-11-24 09:58:31 -0300

    Project finishe

--- bin/projeto_inicial.dart
+++ bin/projeto_inicial.dart
@@ -1,18 +1,46 @@
 void main () {
 
+ //comentario
+  /*comentario de
+  varias linhas
+   */
 
-
-  int idade = 29;
-  double altura = 1.88;
+  var idade = 29;
+  final double altura = 1.88;
   bool geek = true;
-  String nome = 'Jhonathan';
+  const String nome = 'Jhonathan';
+  String apelido = 'John';
+  bool maiorDeIdade;
+  int energia = 100;
 
-  List<dynamic> john = [29, 1.88, true, 'Jhonathan'];
+  //Metodologia para definir se e maior de idade
+  if(idade >= 18){
+    maiorDeIdade = true;
+  } else {
+    maiorDeIdade = false;
+  }
 
-  String frase = 'Eu sou o ${john[3]} \n'
-  'Eu me considero Geek: ${john[2]} \n'
-  'tenho ${john[0]} anos e ${john[1]} de altura';
-  List<String> listaNomes = ['Jhonathan', 'Janny', 'Vera', 'Dida'];
+  //Metodo para descobrir quantas voltas foram dadas
+  for(int i = 1; i < 5; i++){
+    print('conclui $i voltas');
+  }
+
+  //metodo para retirar a energia
+  while (energia > 0){
+    print('Mais uma repetição');
+        energia = energia - 10;
+  }
+
+// ctrl + barra = comenta oque ta selecionado.
+  // print(idade);
+  // List<dynamic> john = [idade, altura, geek, nome];
+  //
+  // String frase = 'Eu sou o ${john[3]} \n'
+  // 'Eu me considero Geek: ${john[2]} \n'
+  // 'tenho ${john[0]} anos e ${john[1]} de altura '
+  //     'Eu sou maior de idade? ${maiorDeIdade}';
+
+
 
 
 
