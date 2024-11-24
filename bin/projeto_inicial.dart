a4a5c65be07f008e37882c1f3091e845c0d55823
Jhonathan <Jhonathan.Engineer@gmail.com>
2024-11-24 08:51:02 -0300

    chore: add types int, double, String and bool

--- bin/projeto_inicial.dart
+++ bin/projeto_inicial.dart
@@ -1,3 +1,11 @@
 void main () {
-  print('Olá Mundo!');
+  int idade = 29;
+  double altura = 1.88;
+  bool geek = true;
+  String nome = 'Jhonathan';
+  String frase = 'Eu sou o $nome \n'
+  'Eu me considero Geek: $geek \n'
+  'tenho $idade anos e $altura de altura';
+
+  print(frase);
 }
